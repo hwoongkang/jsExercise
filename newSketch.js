6b36ce3b6bdbe234fcc79ed7968f726cdb343b5c
Juergen Kirchner <hwoongkang.gmail.com>
2019-08-03 17:28:50 +0900

    acceleration including gravity

--- newSketch.js
+++ newSketch.js
@@ -5,27 +5,27 @@ var x,y,z;
 function setup(){
     paragraph = createP("hwoongkang");
     createCanvas(1920,1080);
-    /*
+    
     x=0;
     y=0;
     z=0;
-    if(window.DeviceOrientationEvent){
+    if(window.DeviceMotionEvent){
         alert("Supported");
-        window.addEventListener("deviceorientation", function(event){
-            x = event.alpha;
-            y = event.beta;
-            z = event.gamma;
+        window.addEventListener("devicemotion", function(event){
+            x = event.accelerationIncludingGravity.x;
+            y = event.accelerationIncludingGravity.y;
+            z = event.accelerationIncludingGravity.z;
         });
       }else{
         alert("DeviceMotionEvent is not supported");
       }
-      */
+      
 }
 function draw(){
-    
+    /*
     x = accelerationX;
     y = accelerationY;
-    z = accelerationZ;
+    z = accelerationZ;*/
     roll = atan2(-y,z);
     pitch = atan2(x, sqrt(y*y+z*z));
     paragraph.elt.style['font-variation-settings'] = `"wght" ${roll}`;
