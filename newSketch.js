38d24bbb88a2c12788c02dd297ce94c5a11f1553
Juergen Kirchner <hwoongkang.gmail.com>
2019-08-03 18:13:35 +0900

    pitch included

--- newSketch.js
+++ newSketch.js
@@ -29,7 +29,6 @@ function draw(){
     background(255);
     roll = atan2(x,-z);
     pitch = atan2(y, sqrt(x*x+z*z));
-    text(roll.toString(),80,80);
-    text(pitch.toString(),100,100);
-    paragraph.elt.style['font-variation-settings'] = `"wght" ${90+180/PI*roll}`;
+    paragraph.elt.style['font-variation-settings'] = `"wght" ${90+180/PI*roll}
+     "wdth" ${180 + 180/PI*pitch}`;
 }
