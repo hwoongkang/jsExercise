9ce4756c6b0be55b9af7aaba7df2c3369869f6e3
Juergen Kirchner <hwoongkang.gmail.com>
2019-08-03 18:20:32 +0900

    shim pyo

--- newSketch.js
+++ newSketch.js
@@ -29,6 +29,6 @@ function draw(){
     background(255);
     roll = atan2(x,-z);
     pitch = atan2(y, sqrt(x*x+z*z));
-    paragraph.elt.style['font-variation-settings'] = `"wght" ${90+180/PI*roll}
+    paragraph.elt.style['font-variation-settings'] = `"wght" ${90+180/PI*roll},
      "wdth" ${180 + 180/PI*pitch}`;
 }
