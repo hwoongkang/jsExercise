2e4b3006b45d3c2f9f9b52c5d03f299dec31ee09
Juergen Kirchner <hwoongkang.gmail.com>
2019-08-03 15:45:47 +0900

    hoxy

--- newSketch.js
+++ newSketch.js
@@ -3,14 +3,20 @@ var roll,pitch;
 var x,y,z;
 
 function setup(){
-    paragraph = createP("hwoongkang");
+    //paragraph = createP("hwoongkang");
+    createCanvas(1920,1080);
 }
 function draw(){
+    /*
     x = accelerationX;
     y = accelerationY;
     z = accelerationZ;
     roll = atan2(-y,z);
     pitch = atan2(x, sqrt(y*y+z*z));
     paragraph.elt.style['font-variation-settings'] = `"wght" ${mouseX}`;
-    
+    */
+   background(255)
+   x=accelerationX;
+   y=random();
+   text(x.toString(),100,100)
 }
