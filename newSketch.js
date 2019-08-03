fadd54030a6f58a27adb6d2b84084a1a62629ef1
Juergen Kirchner <hwoongkang.gmail.com>
2019-08-03 15:13:24 +0900

    ARS

--- newSketch.js
+++ newSketch.js
@@ -1,16 +1,17 @@
-var alpha,beta,gamma;
 var paragraph;
+var roll,pitch;
+var x,y,z;
 
 function setup(){
     paragraph = createP("Leedotype");
     alpha = 0;
 }
 function draw(){
-    paragraph.elt.style['font-variation-settings'] = `"wght" ${90+alpha}`;
+    x = accelerationX;
+    y = accelerationY;
+    z = accelerationZ;
+    roll = atan2(-y,z);
+    pitch = atan2(x, sqrt(y*y+z*z));
+    paragraph.elt.style['font-variation-settings'] = `"wght" ${90+roll}`;
     
 }
-window.addEventListener('deviceorientation',function(e){
-    alpha = e.alpha;
-    beta = e.beta;
-    gamma = e.gamma;
-})
